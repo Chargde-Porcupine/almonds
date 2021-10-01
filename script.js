99ceb7c7440f83ef9407e6707b15fb0c000f5038
chargde_porcupine <64236644+Chargde-Porcupine@users.noreply.github.com>
2021-10-01 10:14:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function vote1(){
   if(document.cookie=="vote=2"){
-    Console.log("switch 1");
+    console.log("switch 1");
     document.cookie = "vote=1"
   } else if(document.cookie===""){
     document.cookie = "vote=1";
@@ -10,7 +10,7 @@ function vote1(){
 }
 function vote2(){
   if(document.cookie=="vote=1"){
-    Console.log("switch 2");
+    console.log("switch 2");
     document.cookie = "vote=2"
   } else if(document.cookie===""){
     document.cookie = "vote=2";
